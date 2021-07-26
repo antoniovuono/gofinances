9efd07714766fbf57056f0c6fe8eed7bcf1f70df
Antonio Vuono <vuono@mswebmaker.com>
2021-07-26 12:46:03 -0300

    Header initied

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 
-import { Container, Title } from './styles'
+import {
+     Container,
+     Header,
+     UserWrapper,
+     UserInfo,
+     Photo,
+     User,
+     UserGrettings,
+     UserName
+ } from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri: 'https://avatars.githubusercontent.com/u/7297243?s=400&v=4'}}/>
+                        <User>
+                            <UserGrettings>Olá,</UserGrettings>
+                            <UserName>Antonio</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,16 +1,64 @@
 import styled from 'styled-components/native';
 
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
 export const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;  
+    flex: 1; 
     background-color: ${({theme}) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-    font-size: 24px;
-    font-weight: bold;
-    color: ${({theme}) => theme.colors.title};
-    font-family: ${({theme}) => theme.fonts.bold};
+export const Header = styled.View`
+
+    background-color: ${({theme}) => theme.colors.primary};
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+
+`;
+
+export const UserWrapper = styled.View`
+
+    width: 100%;
+    padding: 0 24px;
+
+`;
+
+export const UserInfo = styled.View`
+
+    flex-direction: row;
+    align-items: center;
+
+`;
+
+export const Photo = styled.Image`
+
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+
+`;
+
+export const User = styled.View`
+
+    margin-left: 17px;
+
+`;
+
+export const UserGrettings = styled.Text`
+
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+    color: ${({theme}) => theme.colors.shape};
+
+`;
+
+export const UserName  = styled.Text`
+
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(18)}px;
+    color: ${({theme}) => theme.colors.shape};
 
 `;
