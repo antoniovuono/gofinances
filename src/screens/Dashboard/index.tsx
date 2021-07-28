356fa82545a2ef4ec228ed6bb0b8f2ce2856ba59
Antonio Vuono <vuono@mswebmaker.com>
2021-07-28 12:23:26 -0300

    List finalized

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard, TransactionsCardProps } from "../../components/TransactionCard";
 
 import {
      Container,
@@ -11,10 +12,54 @@ import {
      UserGrettings,
      UserName,
      Icon,
-     HighlightCards
- } from './styles'
+     HighlightCards,
+     Transactions,
+     Title,
+     TransactionList
+ } from './styles';
+
+ export interface DataListProps extends TransactionsCardProps {
+     id: string;
+ }
 
 export function Dashboard(){
+
+    const data: DataListProps[] = [
+        {
+        id: '1',
+        type: 'positive',
+        title:"Desenvolvimento de site",
+        amount:"R$ 12.000,00",
+        category:{
+            name: "Vendas",
+            icon: "dollar-sign",
+        },
+        date:"13/04/2020"
+    },
+    {
+        id: '2',
+        type: 'negative',
+        title:"Hamburgueria Pizzy",
+        amount:"R$ 59,00",
+        category:{
+            name: "Alimentação",
+            icon: "coffee",
+        },
+        date:"10/04/2020"
+    },
+    {
+        id: '3',
+        type: 'negative',
+        title:"Aluguel do apartamento",
+        amount:"R$ 1.200,00",
+        category:{
+            name: "Casa",
+            icon: "shopping-bag",
+        },
+        date:"13/04/2020"
+    }
+];
+
     return (
         <Container>
             <Header>
@@ -32,10 +77,24 @@ export function Dashboard(){
             </Header>
        
          <HighlightCards>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard type="up" title="Entrada" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril"/>
+                <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril" />
+                <HighlightCard type="total"  title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
          </HighlightCards>
+ 
+        <Transactions>
+            <Title>Listagem</Title>
+
+            <TransactionList
+            
+                data={data}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <TransactionCard data={item} />}
+            
+            />
+
+        </Transactions>
+        
 
         </Container>
     );
