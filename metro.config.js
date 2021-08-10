18124e32d2f9fc107410a821bda125a983a48233
Antonio Vuono <vuono@mswebmaker.com>
2021-08-10 16:58:36 -0300

    Expo config changed

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// expo v40:
+const { getDefaultConfig } = require("@expo/metro-config");
 
 module.exports = (async () => {
-    const {
-      resolver: { sourceExts, assetExts }
-    } = await getDefaultConfig();
-    return {
-      transformer: {
-        babelTransformerPath: require.resolve("react-native-svg-transformer")
-      },
-      resolver: {
-        assetExts: assetExts.filter(ext => ext !== "svg"),
-        sourceExts: [...sourceExts, "svg"]
-      }
-    };
-  })();
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
